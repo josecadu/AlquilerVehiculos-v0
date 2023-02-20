f83995c291748c19f31555c6927c87f47e79c0a5
jmcab <jmcab@DESKTOP-BRH30L1>
2023-02-20 17:35:53 +0100

    a

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Clientes.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Clientes.java
@@ -1,5 +1,13 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio;
 
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
+
 public class Clientes {
+private  Cliente[] coleccionClientes;
+public Clientes()
+{
+	
+	coleccionClientes = new Cliente[];
+}
 
 }
