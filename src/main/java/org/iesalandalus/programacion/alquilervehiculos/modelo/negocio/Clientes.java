9d256c5c845c6b8e6faa7526934d4bf9b05631f5
jmcab <jmcab@DESKTOP-BRH30L1>
2023-02-22 13:27:11 +0100

    test de clases Turismos y Clientes pasados correctamente

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Clientes.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Clientes.java
@@ -1,13 +1,91 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio;
 
+import java.util.ArrayList;
+import javax.naming.OperationNotSupportedException;
+import java.util.*;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
 
-public class Clientes {
-private  Cliente[] coleccionClientes;
-public Clientes()
-{
-	
-	coleccionClientes = new Cliente[];
-}
 
+public class Clientes {
+	private static List<Cliente> coleccionClientes;
+
+
+	public Clientes(){
+		coleccionClientes = new ArrayList<>();
+	}
+
+
+	
+	public List<Cliente> get() {
+		List <Cliente> nuevaColeccion = new ArrayList<>();
+		nuevaColeccion.addAll(coleccionClientes);
+		return nuevaColeccion;
+	}
+	
+	
+	
+	public int getCantidad(){
+		return coleccionClientes.size();
+	}
+	
+	
+	
+	public void insertar(Cliente cliente) throws OperationNotSupportedException {
+		int indice;
+		indice = coleccionClientes.indexOf(cliente);
+		if (indice !=-1 )
+			throw new OperationNotSupportedException("ERROR: Ya existe un cliente con ese DNI.");
+		if (cliente==null)
+			throw new NullPointerException("ERROR: No se puede insertar un cliente nulo.");
+		else
+			coleccionClientes.add(cliente);	
+	}
+
+
+
+	public Cliente buscar(Cliente cliente){
+		int indice;
+		indice = coleccionClientes.indexOf(cliente);
+		if (cliente==null)
+			throw new NullPointerException("ERROR: No se puede buscar un cliente nulo.");
+		if (indice !=-1)
+			return	coleccionClientes.get(indice);
+		else
+			return null;
+	}
+
+
+
+	public void borrar(Cliente cliente) throws OperationNotSupportedException{
+		int indice;
+		indice = coleccionClientes.indexOf(cliente);
+		if (cliente==null)
+			throw new NullPointerException("ERROR: No se puede borrar un cliente nulo.");
+		if (indice ==-1)
+			throw new OperationNotSupportedException("ERROR: No existe ningún cliente con ese DNI.");
+		else
+			coleccionClientes.remove(indice);
+	}
+
+	
+	
+	public void modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException {
+	
+		if (cliente==null)
+			throw new NullPointerException("ERROR: No se puede modificar un cliente nulo.");
+		if (!coleccionClientes.contains(cliente))
+			throw new OperationNotSupportedException("ERROR: No existe ningún cliente con ese DNI.");
+		else {
+		
+			if (nombre!=null && !nombre.isBlank()) {
+				
+				cliente.setNombre(nombre);
+			}
+				if (telefono!=null && !telefono.isBlank()) {
+					
+				cliente.setTelefono(telefono);
+				}
+		}	
+	}
 }
+  
--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Turismos.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Turismos.java
@@ -1,5 +1,71 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio;
 
-public class Turismos {
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.OperationNotSupportedException;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Turismo;
 
+public class Turismos {
+	private static List<Turismo> coleccionTurismos;
+	
+	public Turismos() {
+		coleccionTurismos = new ArrayList<>();
+	}
+	
+	
+	
+	public List<Turismo> get(){
+		List<Turismo> nuevaColeccion = new ArrayList<>();
+		nuevaColeccion.addAll(coleccionTurismos);
+		return nuevaColeccion;	
+	}
+	
+	
+	
+	public int getCantidad() {
+		return coleccionTurismos.size();
+	}
+	
+	
+	
+	public void insertar(Turismo turismo) throws OperationNotSupportedException {
+		int indice;
+		indice = coleccionTurismos.indexOf(turismo);
+		if(turismo==null)
+			throw new NullPointerException("ERROR: No se puede insertar un turismo nulo.");
+		
+		if (indice !=-1 && turismo!=null)
+			throw new OperationNotSupportedException("ERROR: Ya existe un turismo con esa matrícula.");
+		else
+			coleccionTurismos.add(turismo);	
+	}
+	
+	
+	
+	public Turismo buscar(Turismo turismo) {
+	
+	int indice;
+	indice = coleccionTurismos.indexOf(turismo);
+	if(turismo==null)
+		throw new NullPointerException("ERROR: No se puede buscar un turismo nulo.");
+	
+	if (indice !=-1)
+		return	coleccionTurismos.get(indice);
+	else
+		return null;
+	}
+	
+	
+	
+	public void borrar(Turismo turismo) throws OperationNotSupportedException{
+		int indice;
+		indice = coleccionTurismos.indexOf(turismo);
+		if(turismo==null)
+			throw new NullPointerException("ERROR: No se puede borrar un turismo nulo.");
+		
+		if (indice ==-1)
+			throw new OperationNotSupportedException("ERROR: No existe ningún turismo con esa matrícula.");
+		else
+			coleccionTurismos.remove(indice);
+	}
 }
