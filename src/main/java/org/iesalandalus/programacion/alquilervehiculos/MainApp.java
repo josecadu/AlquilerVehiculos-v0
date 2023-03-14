b0a3edb3c78c40ef420053dbef829d1f7e2a3da1
jmcab <jmcab@DESKTOP-BRH30L1>
2023-03-14 22:31:25 +0100

    modificado vista para ser vista texto creado enum tipoVehiculo y
    realizados cambios necesarios en main

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -1,13 +1,14 @@
 package org.iesalandalus.programacion.alquilervehiculos;
 
 import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.FactoriaFuenteDatos;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.ModeloCascada;
 import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-	ModeloCascada modeloCascada = new ModeloCascada();
+	ModeloCascada modeloCascada = new ModeloCascada(FactoriaFuenteDatos.MEMORIA.crear());
 	Vista vista = new Vista();
 	Controlador controlador= new Controlador(modeloCascada,vista);
 	controlador.comenzar();
